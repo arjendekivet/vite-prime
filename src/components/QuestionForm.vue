6924e8ac4958ff4e8b4302b2942122f444ae65d8
Arjen73 <arjendekivet@gmail.com>
2021-09-08 15:57:26 +0200

    cleanup

--- src/components/QuestionForm.vue
+++ src/components/QuestionForm.vue
@@ -1,38 +1,7 @@
 <template>
   <div class="card">
     <div class="p-fluid p-formgrid p-grid">
-      <div class="p-field p-col-12 p-md-6">
-        <label for="firstname6">Firstname</label>
-        <P-InputText id="firstname6" v-model="value1"></P-InputText>
-      </div>
-      <div class="p-field p-col-12 p-md-6">
-        <label for="lastname6">Lastname</label>
-        <P-InputText id="lastname6" type="text" />
-      </div>
-      <div class="p-field p-col-12 p-md-6">
-        <label for="address">Address</label>
-        <P-InputText id="address" />
-      </div>
-      <div class="p-field p-col-12 p-md-6">
-        <label for="city">City</label>
-        <P-InputText id="city" type="text" />
-      </div>
-      <div class="p-field p-col-12 p-md-6">
-        <label for="state">State</label>
-        <P-Dropdown
-          inputId="state"
-          v-model="selectedState"
-          :options="states"
-          optionLabel="name"
-          placeholder="Select"
-        />
-      </div>
-      <div class="p-field p-col-12 p-md-6">
-        <label for="zip">Zip</label>
-        <P-InputText id="zip" type="text" />
-      </div>
-
-      <div v-for="field in fields" class="p-field p-col-12 p-md-6">
+      <div v-for="field in fields" :class="`p-field p-col-12 p-md-${12 / columns}`">
         <label :for="field.id">{{ field.label }}</label>
         <component
           :is="field.type"
@@ -51,6 +20,10 @@
 import { ref } from 'vue'
 import { Fieldconfig } from '@/types/fieldconfig'
 
+const props = defineProps({
+  columns: { type: Number, default: 2 }
+})
+
 const states = ref([
   { name: 'Arizona', code: 'Arizona' },
   { name: 'California', value: 'California' },
@@ -59,24 +32,28 @@ const states = ref([
   { name: 'Washington', code: 'Washington' }
 ]);
 
-const value = ref()
-const value1 = ref()
 const fieldValues: any = ref<object>({})
-
 const fields = ref<Fieldconfig[]>(
   [
     {
-      id: 'fieldOne',
+      id: 'firstname',
       label: 'Firstname',
       type: 'P-InputText',
-      placeholder: 'placeholder ...'
+      placeholder: 'Firstname'
     },
     {
-      id: 'fieldTwo',
-      label: 'States',
+      id: 'lastname',
+      label: 'Lastname',
+      type: 'P-InputText',
+      placeholder: 'Lastname'
+    },
+    {
+      id: 'state',
+      label: 'State',
       type: 'P-Dropdown',
       options: states.value,
-      optionLabel: "name"
+      optionLabel: "name",
+      placeholder: 'States'
     }
   ]
 )
@@ -92,10 +69,6 @@ const selectedState = ref(null);
       margin-bottom: 1em !important;
     }
   }
-}
-
-.layout-content .content-section.implementation > h3 {
-  font-weight: 600;
 }
 
 textarea {
