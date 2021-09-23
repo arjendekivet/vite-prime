46635492cd7d06eeb194c11eec86fe587f04969c
Arjen73 <arjendekivet@gmail.com>
2021-09-23 11:28:42 +0200

    Add StoryBook and tweak to support alias from vite

---
+++ .storybook/main.js
@@ -0,0 +1,24 @@
+const { loadConfigFromFile, mergeConfig } = require("vite");
+const path = require("path");
+
+// reuse the project's vite.config.js using viteFinal function and
+// you can use the API mergeConfig and loadConfigFromFile exposed by vite
+
+module.exports = {
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  core: {
+    builder: "storybook-builder-vite",
+  },
+  async viteFinal(config, { configType }) {
+    const { config: userConfig } = await loadConfigFromFile(
+      path.resolve(__dirname, "../vite.config.ts")
+    );
+
+    return mergeConfig(config, {
+      ...userConfig,
+      // manually specify plugins to avoid conflict
+      plugins: [],
+    });
+  },
+};
