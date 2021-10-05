1ad81d49da5a464abde994075235f2b28b0ef4e7
Arjen73 <arjendekivet@gmail.com>
2021-10-05 12:27:32 +0200

    Cleanup 7

--- src/components/forms/ExampleFormTwo.vue
+++ src/components/forms/ExampleFormTwo.vue
@@ -1,18 +1,27 @@
 <template>
-  <div>
-    <DynamicFormLayoutTwo
-      v-for="config in formConfig"
-      :config="config"
-      :columns="2"
-      data-type="examples"
-      title="My Title"
-    />
+  <div class="dynamicformlayout">
+    <FormLayoutRecursor v-for="config in formConfig" :config="config"></FormLayoutRecursor>
   </div>
 </template>
 
 <script setup lang="ts">
-import DynamicFormLayoutTwo from '@/components/dynamic-form-layout/DynamicFormLayoutTwo.vue'
-import Fieldconfig from '@/types/fieldconfig'
+import FormLayoutRecursor from '@/components/recursor/FormLayoutRecursor.vue'
 import formConfig from '@/data/FormLayoutOne'
 
 </script>
+
+<style lang="scss">
+.dynamicformlayout {
+  textarea {
+    resize: none;
+  }
+
+  .pi {
+    z-index: 1;
+  }
+
+  .p-field > label {
+    margin-left: 0.25rem;
+  }
+}
+</style>
