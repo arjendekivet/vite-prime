e548d5bd489625a353c26f14697197a1d2cc5140
Arjen73 <arjendekivet@gmail.com>
2021-10-08 09:34:02 +0200

    Restore exampleform to dev

--- src/components/forms/ExampleForm.vue
+++ src/components/forms/ExampleForm.vue
@@ -1,7 +1,43 @@
 <template>
-  <div class="dynamicformlayout">ExampleForm</div>
+  <DynamicForm :fields="fields" data-type="examples" :columns="2" />
 </template>
 
 <script setup lang="ts">
+import DynamicForm from '@/components/dynamic-form/DynamicForm.vue'
+import Fieldconfig from '@/types/fieldconfig'
+import Option from '@/types/Option'
+
+const states: Option[] = [
+  { label: 'Arizona', value: 'Arizona' },
+  { label: 'California', value: 'California' },
+  { label: 'Florida', value: 'Florida' },
+  { label: 'Ohio', value: 'Ohio' },
+  { label: 'Washington', value: 'Washington' }
+];
+
+const fields: Fieldconfig[] =
+  [
+    {
+      id: 'firstname',
+      label: 'Firstname',
+      type: 'P_InputText',
+      placeholder: 'Firstname'
+    },
+    {
+      id: 'lastname',
+      label: 'Lastname',
+      type: 'P_InputText',
+      placeholder: 'Lastname'
+    },
+    {
+      id: 'state',
+      label: 'State',
+      type: 'P_Dropdown',
+      options: states,
+      optionLabel: "label",
+      optionValue: "value",
+      placeholder: 'States'
+    }
+  ]
 
 </script>
